f2c6da234ebc3ccdbefa730b9d51c81891c8cada
freshmanacadamy <freshmanacadamy@gmail.com>
2025-11-11 00:43:36 +0300

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  BOT_TOKEN: process.env.BOT_TOKEN || '8580563953:AAEUQ1TLXpEbnUPlAX5VHwinWmB_rrKc3IU',
-  ADMIN_IDS: process.env.ADMIN_IDS ? process.env.ADMIN_IDS.split(',').map(Number) : [7362758034],
-  DATABASE_PATH: './bot.db',
-  
-  // Bot states
-  botStopped: false,
-  maintenanceMode: false
-};
